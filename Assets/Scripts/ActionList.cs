bb35b8b7b3d653a79e80940c7752bc541acd700f
Christian Gonzales <christian.gonzales@digipen.edu>
2026-02-18 09:47:36 -0800

    turned into list

--- Assets/Scripts/ActionList.cs
+++ Assets/Scripts/ActionList.cs
@@ -2,36 +2,36 @@ using System.Collections.Generic;
 
 public sealed class ActionList
 {
-    private readonly Queue<GameAction> _queue = new Queue<GameAction>();
+    private readonly List<GameAction> _list = new List<GameAction>();
     private GameAction _current;
 
-    public int PendingCount => _queue.Count;
+    public int PendingCount => _list.Count;
     public GameAction Current => _current;
 
     public void Enqueue(GameAction action)
     {
         if (action != null)
-            _queue.Enqueue(action);
+            _list.Add(action); // Add to end (like Enqueue)
     }
 
     public void Clear()
     {
-        _queue.Clear();
+        _list.Clear();
         _current = null;
     }
 
-
     public void Tick(float deltaTime)
     {
-
         if (_current == null)
         {
-            if (_queue.Count == 0) return;
-            _current = _queue.Dequeue();
+            if (_list.Count == 0)
+                return;
+
+            _current = _list[0];   // Take first item
+            _list.RemoveAt(0);     // Remove it (like Dequeue)
         }
 
         _current.Update(deltaTime);
-
 
         if (_current.Done)
             _current = null;
