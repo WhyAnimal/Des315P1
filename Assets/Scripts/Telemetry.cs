dabcde16cf92ccbb31e299a07de8f9989c631cec
Christian Gonzales <christian.gonzales@digipen.edu>
2026-02-20 10:54:55 -0800

    tele just left

---
+++ Assets/Scripts/Telemetry.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telemetry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //info wants
+
+    //player cards played, player hand won
+    //number times esc was pressed, and how many menu options pressed
+    //performance data mean, median and worst
+}
